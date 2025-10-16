3449ef690c31b92e83ee70f1b802d9df5905727d
MolchanovIV <Tyuiu.MolchanovIV@outlook.com>
2025-10-16 16:38:11 +0500

    Task5 - Fix

--- Tyuiu.MolchanovIV.Sprint2.Task5.V5.Lib/DataService.cs
+++ Tyuiu.MolchanovIV.Sprint2.Task5.V5.Lib/DataService.cs
@@ -8,18 +8,18 @@ namespace Tyuiu.MolchanovIV.Sprint2.Task5.V5.Lib
         {
             switch (value)
             {
-                case 6: return "Шестерка";
-                case 7: return "Семерка";
-                case 8: return "Восьмерка";
-                case 9: return "Девятка";
-                case 10: return "Десятка";
-                case 11: return "Валет";
-                case 12: return "Дама";
-                case 13: return "Король";
-                case 14: return "Туз";
+                case 6: return "шестерка";
+                case 7: return "семерка";
+                case 8: return "восьмерка";
+                case 9: return "девятка";
+                case 10: return "десятка";
+                case 11: return "валет";
+                case 12: return "дама";
+                case 13: return "король";
+                case 14: return "туз";
             }
 
-            return "Ошибка";
+            return "ошибка";
 
         }
     }
